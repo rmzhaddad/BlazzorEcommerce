7ba49352d513f97bd615ae57d5d5d2222af886be
ramzi Haddad <rmzhaddad1@gmail.com>
2024-05-08 05:05:39 -0400

    Add a search bar to the client

--- BlazzorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazzorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -16,6 +16,7 @@ namespace BlazzorEcommerce.Client.Services.ProductService
        
 
         public List<Product> products { get; set; }=new List<Product>();
+        public string Message { get; set; } = "Loading Products...";
 
         public async Task GetProducts(string? categoryUrl=null)
         {
@@ -34,6 +35,22 @@ namespace BlazzorEcommerce.Client.Services.ProductService
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
             return result;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if(result!=null && result.Data!=null)
+            { products = result.Data; }
+            if (products.Count == 0) Message = "No products found.";
+            ProductsChanged?.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
+
         }
     }
 }
--- BlazzorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazzorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -5,5 +5,7 @@
         Task<ServiceResponse<List<Product>>> GetProductsAsync();
         Task<ServiceResponse<Product>> GetProductAsync(int productId);
         Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
+        Task<ServiceResponse<List<Product>>> SearchProducts(string SearchText);
+        Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string SearchText);
     }
 }
